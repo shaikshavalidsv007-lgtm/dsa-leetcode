733f8c389a5290c021635f58cfd5bdc1f80209eb
shekshavali <shaikshavalidsv007@gmail.com>
2026-04-14 07:16:25 +0530

    Time: 2 ms (22.46%), Space: 63.3 MB (9.08%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,5 +1,5 @@
 class Solution {
-    public void rotate(int[] nums, int k) {
+    public void rotate_optimal(int[] nums, int k) {
 
         int n = nums.length;
         k =k%n;
@@ -17,6 +17,41 @@ class Solution {
             nums[end] = temp;
             start++;
             end--;
+        }
+    }
+
+
+    public void rotate_brute(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
+        for(int i = 0; i < k; i++) {
+
+            int last = nums[n - 1];
+
+            for(int j = n - 1; j > 0; j--) {
+                nums[j] = nums[j - 1];
+            }
+
+            nums[0] = last;
+        }
+    }
+
+
+
+
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
+        int[] temp = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            temp[(i + k) % n] = nums[i];
+        }
+
+        for(int i = 0; i < n; i++) {
+            nums[i] = temp[i];
         }
     }
 }
