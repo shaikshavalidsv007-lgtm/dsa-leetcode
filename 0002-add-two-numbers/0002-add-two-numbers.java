e6d83a6e7a31f71255baf6d9c0f5d7fb330eea5c
shekshavali <shaikshavalidsv007@gmail.com>
2026-06-09 22:57:05 +0530

    Time: 1 ms (100%), Space: 46.4 MB (75.07%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers_first(ListNode l1, ListNode l2) {
+
+        ListNode res = new ListNode(0);
+        ListNode temp = res;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int sum = 0 + carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            
+            carry = sum / 10;
+            sum %= 10;
+            temp.next = new ListNode(sum);
+            temp = temp.next;
+            
+        }
+
+        if(carry ==1) {
+            temp.next = new ListNode(1);
+        }
+        return res.next;
+        
+    }
+
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+         ListNode res = null;
+        ListNode temp = res;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = 0;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            sum %= 10;
+            if (res == null) {
+                res = new ListNode(sum);
+                temp = res;
+            } else {
+                temp.next = new ListNode(sum);
+                temp = temp.next;
+            }
+        }
+        return res;
+        
+    }
+}
